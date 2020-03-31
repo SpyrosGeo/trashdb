a59ef8442a133e7c67332668ddbf4126ef3fceb8
SpyrosGeo <xarodasimba@gmail.com>
2020-04-01 01:00:35 +0300

    implemented a reusable debounce

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const KEY = "41101192";
 const input = document.querySelector('input')
-const fetchData = async(searchTerm)=>{
-    const response = await axios.get("http://www.omdbapi.com/",{
-        params:{
-            apikey:KEY,
-            s:searchTerm
+const fetchData = async (searchTerm) => {
+    const response = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: KEY,
+            s: searchTerm
         }
-        });
+    });
     console.log(response.data)
 }
 
-//debounce api requests for every value on input
-let timeoutId;
-const onInput = e =>{
-    if(timeoutId){
-        clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(()=>{
-        fetchData(e.target.value)
 
-    },1000)
+
+//debounce 
+const debounce = (func) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func(...args)
+        }, 1000)
+    }
 }
-input.addEventListener('input',onInput);
+
+
+const onInput = debounce(e => {
+    if(e.target.value){
+        fetchData(e.target.value)
+    }
+})
+input.addEventListener('input', onInput);
