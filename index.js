1b97d67950b44ee92fe5bcfc1369b60a55c5350c
SpyrosGeo <xarodasimba@gmail.com>
2020-04-01 02:35:47 +0300

    added expanded summary rendering

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ const fetchData = async (searchTerm) => {
     }
     return (response.data.Search)
 }
-const onMovieSelect = async (movieId) =>{
-    const res = await axios.get("http://www.omdbapi.com/",{
-        params:{
-            apikey:KEY,
-            i:movieId
+const onMovieSelect = async (movieId) => {
+    const res = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: KEY,
+            i: movieId
         }
     })
-    console.log(res.data)
+    return res.data
 }
 const root = document.querySelector('.autocomplete');
 
@@ -42,44 +42,64 @@ const dropdown = document.querySelector('.dropdown')
 const resultsWrapper = document.querySelector('.results');
 
 const onInput = async e => {
-    
-        const movies = await fetchData(e.target.value)
-         console.log(movies.length)
-        if(!movies.length){
-            dropdown.classList.remove('is-active')
-            return;
-        }
-        resultsWrapper.innerHTML ='';
 
-        dropdown.classList.add('is-active')
-        for (let i in movies) {
-            let movie = movies[i]
-            //create an anchor for every movie 
-            const option = document.createElement('a');
-            const imgSrc = movie.Poster ==='N/A'?'':movie.Poster
-            option.classList.add('dropdown-item')
-            option.innerHTML = `
+    const movies = await fetchData(e.target.value)
+    if (!movies.length) {
+        dropdown.classList.remove('is-active')
+        return;
+    }
+    resultsWrapper.innerHTML = '';
+
+    dropdown.classList.add('is-active')
+    for (let i in movies) {
+        let movie = movies[i]
+        //create an anchor for every movie 
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
+        option.classList.add('dropdown-item')
+        option.innerHTML = `
             <img src="${imgSrc}"/>
             ${movie.Title}
             `;
-            //replace input value text with the selected movie Title
-            option.addEventListener('click',async()=>{
-                //close the dropdown
-                dropdown.classList.remove('is-active')
-                input.value = movie.Title
-                const details = await onMovieSelect(movie.imdbID)
-                
-            })
-            resultsWrapper.appendChild(option)
-        }
-
+        //replace input value text with the selected movie Title
+        option.addEventListener('click', async () => {
+            //close the dropdown
+            dropdown.classList.remove('is-active')
+            input.value = movie.Title
+            const details = await onMovieSelect(movie.imdbID)
+            document.querySelector('#summary').innerHTML = movieTemplate(details)
+        })
+        resultsWrapper.appendChild(option)
     }
+
+}
 
 input.addEventListener('input', debounce(onInput, 500));
 
 
-document.addEventListener('click',e =>{
-    if(!root.contains(e.target)){
+document.addEventListener('click', e => {
+    if (!root.contains(e.target)) {
         dropdown.classList.remove('is-active')
     }
 })
+
+
+const movieTemplate = movieDetail => {
+    const {Poster,Title,Genre,Plot} = movieDetail
+    return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+        <img src="${Poster}">
+        </p>
+      </figure>
+      <div class="media-content">
+      <div class="content">
+        <h1>${Title}</h1>
+        <h4>${Genre}</h4>
+        <p>${Plot}</p>
+      </div>
+      </div>
+    </article>
+    `
+}
